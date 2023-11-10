4610357e56a39e0ef25d001d54f433ee6fa9158a
SuperKiment <kiment2002@gmail.com>
2023-11-10 15:21:11 +0100

    login, logout

--- produits/views.py
+++ produits/views.py
@@ -2,21 +2,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Produit, Categorie
+from django.contrib.auth.decorators import login_required
 
-
+    
+@login_required(login_url='/accounts/login/')
 def index(request):
     produits = Produit.objects.all()
     context = {'produits': produits}
     return render(request, 'produits/index.html.twig', context)
     
+@login_required(login_url='/accounts/login/')
 def produit_individuel(request, id_produit):
     text = "<h1>Hello world</h1><p>Benjoueuuuuur</p>"
     return HttpResponse(text)
 
+@login_required(login_url='/accounts/login/')
 def panier(request, number):
     text = "lez goooo %d"%number
     return HttpResponse(text)
 
+@login_required(login_url='/accounts/login/')
 def categories(request, libelle = None):
     if (libelle == None):
         return render(request, 'produits/categories.html.twig', {
