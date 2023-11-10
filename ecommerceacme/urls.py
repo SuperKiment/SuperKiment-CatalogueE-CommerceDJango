b26713ec5e110d02bf587cf80ea11087dc7b1edc
Origine0 <antoine244smite@gmail.com>
2023-11-10 10:32:30 +0100

    Redirection index vers produits

--- ecommerceacme/urls.py
+++ ecommerceacme/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include('produits.urls')),
     path('produits/', include('produits.urls')),
+    path('admin/', admin.site.urls),
 ]
